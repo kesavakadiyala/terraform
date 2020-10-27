0f8b9c785248d2fd2d83e3476e09058787409158
= <=>
2020-10-27 10:13:30 +0530

    Added natgateways

--- project/terraform-vpc/gateways.tf
+++ project/terraform-vpc/gateways.tf
@@ -1,7 +1,17 @@
 resource "aws_internet_gateway" "igw" {
   vpc_id            = aws_vpc.main.id
-  tags = {
+  tags              = {
     Name            = "InternetGateWay-${var.ENV}"
     Environment     = var.ENV
   }
+}
+
+resource "aws_eip" "nat" {
+  vpc               = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count             = length(aws_subnet.public_subnet.*.id)
+  allocation_id     = aws_eip.nat.id
+  subnet_id         = element(aws_subnet.private_subnet.*.id, count.index)
 }
--- project/terraform-vpc/route.tf
+++ project/terraform-vpc/route.tf
@@ -4,7 +4,7 @@ resource "aws_route_table" "public" {
     cidr_block     = var.IGW_CIDR
     gateway_id     = aws_internet_gateway.igw.id
   }
-  tags = {
+  tags             = {
     Name           = "public-route-table"
     Environment    = var.ENV
   }
@@ -15,3 +15,14 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
 }
+
+/*resource "aws_route_table" "private" {
+  count         = length(aws_nat_gateway.ngw.*.id)
+  vpc_id        = aws_vpc.main.id
+  route {
+    gateway_id = element(aws_nat_gateway.ngw.id, count.index)
+  }
+  tags         = {
+    Name       = "Private-route-table"
+  }
+}*/
